1ee2898ddae19fac0776362d2c54dd350645e1f7
Aaron Cope <acope@ddcadvocacy.com>
2020-02-20 17:43:42 -0600

    adding src and test dirs.

---
+++ src/Salutation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dependency
+{
+    public class Salutation
+    {
+        public string Compose(string audience) 
+        {
+            if (string.IsNullOrWhiteSpace(audience)) 
+            {
+                throw new ArgumentNullException("You need to address an audience.");
+            }
+
+            //TODO: make sure the audience is not a swear word.
+
+            return "Hello " + audience;
+            // return 
+            // return $"Hello {audience}";
+        }
+    }
+}
---
+++ test/SalutationTests.cs
@@ -0,0 +1,61 @@
+using System;
+using Xunit;
+using dependency;
+
+namespace dependency.test
+{
+    public class SalutationTests
+    {
+        #region construction & formatting tests
+        
+        [Fact]
+        public void ShouldStartWithTheHelloGreetingKeyword()
+        {
+            var salutation = initSalutation();
+            Assert.StartsWith("Hello", salutation.Compose("there"));
+        }
+
+        [Fact]
+        public void ShouldEndTheGreetingWithSuppliedAudience()
+        {
+            var salutation = initSalutation();
+            var audience = "folks";
+            Assert.EndsWith(audience, salutation.Compose(audience));
+        }
+
+        #endregion
+
+        #region boundary testing
+
+        [Fact]
+        public void ShouldThrowExceptionWhenProvidedNullAudience()
+        {
+            var salutation = initSalutation();
+            Assert.Throws<ArgumentNullException>(() => salutation.Compose(null));
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenProvidedEmptyAudience()
+        {
+            var salutation = initSalutation();
+            Assert.Throws<ArgumentNullException>(() => salutation.Compose(string.Empty));
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenProvidedWhitespaceAudience()
+        {
+            var salutation = initSalutation();
+            Assert.Throws<ArgumentNullException>(() => salutation.Compose("  "));
+        }
+
+        #endregion
+
+        #region factories/helpers/initializers
+
+        private Salutation initSalutation() {
+            return new Salutation();
+        }
+        
+        #endregion
+    }
+}
