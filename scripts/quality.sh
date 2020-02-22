5b00f6ff49ef45e40bb7b2488cd8ced4c8533702
Aaron Cope <acope@ddcadvocacy.com>
2020-02-22 12:58:49 -0600

    perhaps a better way to handle the branch specification?

--- scripts/quality.sh
+++ scripts/quality.sh
@@ -48,9 +48,9 @@ done
 #- Main function
 function main {
     dir="$(pwd)"
-    setBranchSpecifier    
+    setBranchSpecifier $SONAR_BRANCH_NAME
     version=$(parseCsProjVersion "src/$SONAR_PROJECT_NAME.csproj")
-    dotnet sonarscanner begin /o:"${SONAR_ORGANIZATION}" /k:"${SONAR_PROJECT_KEY}" /n:"${SONAR_PROJECT_NAME}" /v:"${version}" /d:sonar.host.url="${SONAR_HOST_URL}" /d:sonar.login="${SONAR_LOGIN_TOKEN}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*,test/**/*" /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml"$BRANCH_SPECIFIER
+    dotnet sonarscanner begin /o:"${SONAR_ORGANIZATION}" /k:"${SONAR_PROJECT_KEY}" /n:"${SONAR_PROJECT_NAME}" /v:"${version}" /d:sonar.host.url="${SONAR_HOST_URL}" /d:sonar.login="${SONAR_LOGIN_TOKEN}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*,test/**/*" /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml" $BRANCH_SPECIFIER
     dotnet restore
     dotnet build
     dotnet test ./test/*.test.csproj --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
@@ -65,10 +65,12 @@ function main {
 #- for the sonar
 #- cli
 function setBranchSpecifier {
+    echo "THE BRANCH NAME IS: $1"
+    local branch="/d:sonar.branch.name=$1"
     if [ $1 = "master" ]; then
-        BRANCH_SPECIFIER=""
+        BRANCH_SPECIFIER="$branch"
     else
-        BRANCH_SPECIFIER=" /d:sonar.branch.name=$1"
+        BRANCH_SPECIFIER="$branch /d:sonar.branch.target=master"
     fi
 }
 
