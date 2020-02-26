286eab6c38e85b0e9b8189fa0073cb4d78f2c10a
Aaron Cope <acope@ddcadvocacy.com>
2020-02-26 14:33:43 -0600

    more parameter passing for prs, debugging to ensure everything passes correctly

--- scripts/quality.sh
+++ scripts/quality.sh
@@ -32,8 +32,20 @@ case $key in
     SONAR_PROJECT_NAME="$2"
     shift #past arg
     ;;
-    -b|--sonar-branch-name)
-    SONAR_BRANCH_NAME="$2"
+    -g|--github-repository-identifier)
+    GITHUB_REPOSITORY_IDENTIFIER="$2"
+    shift #past arg
+    ;;
+    -r|--github-pullrequest-branch-ref)
+    GITHUB_PULLREQUEST_BRANCH_REF="$2"
+    shift #past arg
+    ;;
+    -b|--github-branch-name)
+    GITHUB_BRANCH_NAME="$2"
+    shift #past arg
+    ;;
+    -p|--github-pullrequest-number)
+    GITHUB_PULLREQUEST_NUMBER="$2"
     shift #past arg
     ;;
     *)
@@ -48,24 +60,31 @@ done
 #- Main function
 function main {
     dir="$(pwd)"
-    setBranchSpecifier $SONAR_BRANCH_NAME
+    setBranchSpecifier $GITHUB_BRANCH_NAME
     version=$(parseCsProjVersion "src/$SONAR_PROJECT_NAME.csproj")
 
-    echo "sonar url: ${SONAR_HOST_URL}"
-    echo "sonar org: ${SONAR_ORGANIZATION}"
-    echo "sonar project key: ${SONAR_PROJECT_KEY}"
-    echo "sonar project name: ${SONAR_PROJECT_NAME}"
+    echo "===./quality.sh PARAMETER VALUES==="
+    echo "(-u) sonar url: ${SONAR_HOST_URL}"
+    echo "(-t) sonar token: ***masked for your protection***"
+    echo "(-o) sonar org: ${SONAR_ORGANIZATION}"
+    echo "(-k) sonar project key: ${SONAR_PROJECT_KEY}"
+    echo "(-n) sonar project name: ${SONAR_PROJECT_NAME}"
+    echo "(-g) github repository identifier: ${GITHUB_REPOSITORY_IDENTIFIER}"
+    echo "(-r) github branch ref: ${GITHUB_PULLREQUEST_BRANCH_REF}"
+    echo "(-b) git branch name: ${GITHUB_BRANCH_NAME}"
+    echo "(-p) git branch pull request number: ${GITHUB_PULLREQUEST_NUMBER}"
+
     echo "sonar project version: ${version}"
-    echo "git branch: ${BRANCH_SPECIFIER}"
+    echo "branch specifier: ${BRANCH_SPECIFIER}"
 
     # doc for sonarcloud analysis: https://sonarcloud.io/documentation/analysis/overview/
 
-    dotnet sonarscanner begin /o:"${SONAR_ORGANIZATION}" /k:"${SONAR_PROJECT_KEY}" /n:"${SONAR_PROJECT_NAME}" /v:"${version}" /d:sonar.host.url="${SONAR_HOST_URL}" /d:sonar.login="${SONAR_LOGIN_TOKEN}" /d:sonar.language=cs /d:sonar.exclusions=**/bin/**/*,**/obj/**/*,test/**/* /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml" "$BRANCH_SPECIFIER"
-        dotnet restore
-        dotnet build
-        dotnet test ./test/*.test.csproj --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
-    dotnet sonarscanner end /d:sonar.login="${SONAR_LOGIN_TOKEN}"
-    rm -f lcov.info lcov.opencover.xml
+    #dotnet sonarscanner begin /o:"${SONAR_ORGANIZATION}" /k:"${SONAR_PROJECT_KEY}" /n:"${SONAR_PROJECT_NAME}" /v:"${version}" /d:sonar.host.url="${SONAR_HOST_URL}" /d:sonar.login="${SONAR_LOGIN_TOKEN}" /d:sonar.language=cs /d:sonar.exclusions=**/bin/**/*,**/obj/**/*,test/**/* /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml" "$BRANCH_SPECIFIER"
+    #    dotnet restore
+    #    dotnet build
+    #    dotnet test ./test/*.test.csproj --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
+    #dotnet sonarscanner end /d:sonar.login="${SONAR_LOGIN_TOKEN}"
+    #rm -f lcov.info lcov.opencover.xml
 }
 #---------------
 
@@ -76,15 +95,14 @@ function main {
 #- cli
 function setBranchSpecifier {
     local basis="/d:sonar.branch.name=$1"
-    echo "$basis"
-    #if [ $1 = "master" ]; then
-    #    BRANCH_SPECIFIER="$basis"
-    #else
-    #    BRANCH_SPECIFIER="$basis /d:sonar.pullrequest.github.repository=$GITHUB_REPOSITORY_IDENTIFIER \
-    #    /d:sonar.pullrequest.key=$PULL_REQUEST_KEY \
-    #    /d:sonar.pullrequest.branch=$PULL_REQUEST_BRANCH \
-    #    /d:sonar.pullrequest.base=master"
-    #fi
+    if [ $1 = "master" ]; then
+        BRANCH_SPECIFIER="$basis"
+    else
+        BRANCH_SPECIFIER="$basis /d:sonar.pullrequest.github.repository=$GITHUB_REPOSITORY_IDENTIFIER \
+        /d:sonar.pullrequest.key=$GITHUB_PULLREQUEST_NUMBER \
+        /d:sonar.pullrequest.branch=$GITHUB_PULLREQUEST_BRANCH_REF \
+        /d:sonar.pullrequest.base=master"
+    fi
 }
 
 #---------------
