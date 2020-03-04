53f9fb25c8b91bf49a630d1753d8f505bd1e7d8d
Aaron Cope <acope@ddcadvocacy.com>
2020-03-04 15:34:58 -0600

    revising workflow to do minimal steps and abstract those steps to a ci bash script.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,60 +1,16 @@
 #!/bin/bash
 
-BRANCH_SPECIFIER=""
+#---------------
+#- script-wide 
+#- constants and 
+#- variables
+#-
+SONAR_URL="https://sonarcloud.io"
+ORGANIZATION=""
+SONAR_PROJECT_NAME=""
+
 
 #---------------
-#- argument parsing
-#---------------
-args=()
-
-while [[ $# > 0 ]]
-do
-key="$1"
-
-case $key in
-    -u|--sonar-host-url)
-    SONAR_HOST_URL="$2"
-    shift #past arg
-    ;;
-    -t|--sonar-login-token)
-    SONAR_LOGIN_TOKEN="$2"
-    shift #past arg
-    ;;
-    -o|--sonar-organization)
-    SONAR_ORGANIZATION="$2"
-    shift #past arg
-    ;;
-    -k|--sonar-project-key)
-    SONAR_PROJECT_KEY="$2"
-    shift #past arg
-    ;;
-    -n|--sonar-project-name)
-    SONAR_PROJECT_NAME="$2"
-    shift #past arg
-    ;;
-    -g|--github-repository-identifier)
-    GITHUB_REPOSITORY_IDENTIFIER="$2"
-    shift #past arg
-    ;;
-    -r|--github-pullrequest-branch-ref)
-    GITHUB_PULLREQUEST_BRANCH_REF="$2"
-    shift #past arg
-    ;;
-    -b|--github-branch-name)
-    GITHUB_BRANCH_NAME="$2"
-    shift #past arg
-    ;;
-    -p|--github-pullrequest-number)
-    GITHUB_PULLREQUEST_NUMBER="$2"
-    shift #past arg
-    ;;
-    *)
-        #unknown option
-        args+=($1)
-    ;;
-esac
-shift # past arg or value
-done
 
 #---------------
 #- sets the branch
@@ -104,27 +60,38 @@ function sonar {
     version=$(parseCsProjVersion "src/$SONAR_PROJECT_NAME.csproj")
 
     echo "===./quality.sh PARAMETER VALUES==="
-    echo "(-u) sonar url: ${SONAR_HOST_URL}"
-    echo "(-t) sonar token: ***masked for your protection***"
-    echo "(-o) sonar org: ${SONAR_ORGANIZATION}"
-    echo "(-k) sonar project key: ${SONAR_PROJECT_KEY}"
-    echo "(-n) sonar project name: ${SONAR_PROJECT_NAME}"
-    echo "(-g) github repository identifier: ${GITHUB_REPOSITORY_IDENTIFIER}"
-    echo "(-r) github branch ref: ${GITHUB_PULLREQUEST_BRANCH_REF}"
-    echo "(-b) git branch name: ${GITHUB_BRANCH_NAME}"
-    echo "(-p) git branch pull request number: ${GITHUB_PULLREQUEST_NUMBER}"
+    echo "sonar url: ${SONAR_URL}"
+    echo "sonar org: ${ORGANIZATION}"
+    echo "sonar project key: ${SONAR_PROJECT_KEY}"
+    echo "sonar project name: ${SONAR_PROJECT_NAME}"
+    echo "github repository identifier: ${GITHUB_REPOSITORY}"
+    echo "github branch ref: ${GITHUB_PULLREQUEST_BRANCH_REF}"
+    echo "git branch name: ${GITHUB_BRANCH_NAME}"
+    echo "git branch pull request number: ${GITHUB_PULLREQUEST_NUMBER}"
 
     echo "sonar project version: ${version}"
     echo "branch specifier: ${BRANCH_SPECIFIER}"
 
     # doc for sonarcloud analysis: https://sonarcloud.io/documentation/analysis/overview/
 
-    dotnet sonarscanner begin /o:"${SONAR_ORGANIZATION}" /k:"${SONAR_PROJECT_KEY}" /n:"${SONAR_PROJECT_NAME}" /v:"${version}" /d:sonar.host.url="${SONAR_HOST_URL}" /d:sonar.login="${SONAR_LOGIN_TOKEN}" /d:sonar.language=cs /d:sonar.exclusions=**/bin/**/*,**/obj/**/*,test/**/* /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml" ${BRANCH_SPECIFIER}
+    dotnet sonarscanner begin /o:"${ORGANIZATION}" /k:"${SONAR_PROJECT_KEY}" /n:"${SONAR_PROJECT_NAME}" /v:"${version}" /d:sonar.host.url="${SONAR_URL}" /d:sonar.login="${SONAR_TOKEN}" /d:sonar.language=cs /d:sonar.exclusions=**/bin/**/*,**/obj/**/*,test/**/* /d:sonar.cs.opencover.reportsPaths="${dir}/lcov.opencover.xml" ${BRANCH_SPECIFIER}
         dotnet restore
         dotnet build
         dotnet test ./test/*.test.csproj --no-build /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
-    dotnet sonarscanner end /d:sonar.login="${SONAR_LOGIN_TOKEN}"
+    dotnet sonarscanner end /d:sonar.login="${SONAR_TOKEN}"
     rm -f lcov.info lcov.opencover.xml
+}
+#---------------
+
+#---------------
+function setScriptEnvironmentVariables {
+    local github_repository_key="$GITHUB_REPOSITORY" 
+    ORGANIZATION=${github_repository_key%%/*} # retain the part before the last slash 
+    SONAR_PROJECT_NAME=${github_repository_key##*/}  # retain the part after the last slash
+    SONAR_PROJECT_KEY="${ORGANIZATION}_${SONAR_PROJECT_NAME}"
+    GITHUB_PULLREQUEST_BRANCH_REF="$GITHUB_REF"
+    GITHUB_BRANCH_NAME=${GITHUB_PULLREQUEST_BRANCH_REF##*/}  # retain the part after the last slash 
+    GITHUB_PULLREQUEST_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 }
 #---------------
 
@@ -139,17 +106,20 @@ function main {
     dotnet test ./test/*.csproj
 
     # quality analysis
+    dotnet tool install dotnet-sonarscanner --tool-path "$HOME/.dotnet/tools"
+    export PATH="$PATH:$HOME/.dotnet/tools"
     sonar
 
     # package.
     dotnet pack ./src/*.csproj --configuration Release
 
     # add nuget source
-    nuget sources Add -Name "github" -Source "https://nuget.pkg.github.com/$SONAR_ORGANIZATION/index.json" -Username "aaroncope@gmail.com" -Password ${{secrets.GITHUB_TOKEN}}
+    nuget sources Add -Name "github" -Source "https://nuget.pkg.github.com/$ORGANIZATION/index.json" -Username "aaroncope@gmail.com" -Password ${{secrets.GITHUB_TOKEN}}
 
     # push
     nuget push -Source "github" ./src/**/*/*.nupkg -SkipDuplicate
 }
 #---------------
 
+setScriptEnvironmentVariables
 main
